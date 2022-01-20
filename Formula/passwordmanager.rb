f0e34ee6b82baf2d88be963fa004bab7122d0504
tax25 <andreatax25@gmail.com>
2022-01-20 21:27:48 +0100

    Fixed a typo

--- Formula/passwordmanager.rb
+++ Formula/passwordmanager.rb
@@ -1,7 +1,6 @@
 class Passwordmanager < Formula
   desc "Simple password manager written in C++"
   homepage "https://github.com/tax25/homebrew-passwordmanager"
-  url ""
   url "https://github.com/tax25/homebrew-passwordmanager/releases/download/v1.1.0/passwordmanager-mac.tar.gz"
   sha256 "bcb88169681052ffd66395b1267ddd09d105f4dcbd2a3f6762e5662ba7f4c992"
   version "1.1.0"
