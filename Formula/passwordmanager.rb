eca00e59e81e1844cd7d33161bc3d01738fa4cb5
tax25 <andreatax25@gmail.com>
2022-01-20 21:53:10 +0100

    I hope now homebrew works

--- Formula/passwordmanager.rb
+++ Formula/passwordmanager.rb
@@ -2,7 +2,7 @@ class Passwordmanager < Formula
   desc "Simple password manager written in C++"
   homepage "https://github.com/tax25/homebrew-passwordmanager"
   url "https://github.com/tax25/homebrew-passwordmanager/releases/download/v1.1.0/passwordmanager-mac.tar.gz"
-  sha256 "bcb88169681052ffd66395b1267ddd09d105f4dcbd2a3f6762e5662ba7f4c992"
+  sha256 "1a6f5ea7dcc21d074b0ffaca34cc5dbfd126dba567398bd282aaf45e8d5d2d3d"
   version "1.1.0"
 
   def install
