744f5cc653000302fea97c55ba46c1884d7e709f
tax25 <andreatax25@gmail.com>
2022-01-27 10:31:44 +0100

    Modified Formula

--- Formula/passwordmanager.rb
+++ Formula/passwordmanager.rb
@@ -1,11 +1,11 @@
 class Passwordmanager < Formula
   desc "Simple password manager written in C++"
   homepage "https://github.com/tax25/homebrew-passwordmanager"
-  url "https://github.com/tax25/homebrew-passwordmanager/releases/download/v1.1.0/passwordmanager-mac.tar.gz"
-  sha256 "1a6f5ea7dcc21d074b0ffaca34cc5dbfd126dba567398bd282aaf45e8d5d2d3d"
-  version "1.1.0"
+  url "https://github.com/tax25/homebrew-passwordmanager/releases/download/v1.1.1/pwdManager-mac.tar.gz"
+  sha256 "8085890cf55de8f7ec7a11cd1227415d014cf13e6ae0a344679e568a113f7b9f"
+  version "1.1.1"
 
   def install
-    bin.install "passwordmanager"
+    bin.install "pwdManager"
   end
 end
